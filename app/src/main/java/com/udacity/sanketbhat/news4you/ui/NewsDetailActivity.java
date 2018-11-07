c2e541d5f74eb18a17c025fbf0a4135f470de3f1
SanketBhat <sanket.r.bhat.u@gmail.com>
2018-11-08 00:20:22 +0530

    New color scheme * Deleted unused menu item

--- app/src/main/java/com/udacity/sanketbhat/news4you/ui/NewsDetailActivity.java
+++ app/src/main/java/com/udacity/sanketbhat/news4you/ui/NewsDetailActivity.java
@@ -11,7 +11,6 @@ import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
@@ -75,12 +74,6 @@ public class NewsDetailActivity extends AppCompatActivity implements View.OnClic
         }
 
         context.startActivity(intent);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_news_detail, menu);
-        return true;
     }
 
     @Override
